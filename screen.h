94659d3784ca50fd45e2aced787581353dcc7085
Jeremy Russel Gatdula <jga137@uclive.ac.nz>
2021-10-11 19:47:42 +1300

    Add new file

---
+++ screen.h
@@ -0,0 +1,23 @@
+#ifndef SCREEN_H
+#define SCREEN_H
+
+#include "system.h"
+
+#define SCREEN_WIDTH 5
+#define SCREEN_HEIGHT 7
+#define SCREEN_MAX_PRELOAD 5
+
+// Lits given column with given pattern
+void show_column(uint8_t row_pattern, uint8_t current_column);
+// Displays the screen
+void screen_update(void);
+// Returns true/false if given pixel is lit
+bool screen_pixel_get(uint8_t col, uint8_t row);
+// Moves screen up
+void screen_up(void);
+// Initialises screen/display
+void screen_init(void);
+
+#endif //SCREEN_H
+
+
