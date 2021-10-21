c2d48247862e5e7321a514d6b4d5d1ad27906f3f
Jeremy Russel Gatdula <jga137@uclive.ac.nz>
2021-10-21 13:19:38 +1300

    Changed Game over function

--- player.c
+++ player.c
@@ -3,11 +3,6 @@
 #include "player.h"
 #include "screen.h"
 #include <stdlib.h>
-#include <string.h>
-#include "../fonts/font5x7_1.h"
-#include "pacer.h"
-#include "tinygl.h"
-
 
 
 player_t move_forward(player_t player)
@@ -16,7 +11,6 @@ player_t move_forward(player_t player)
 		return player;
 	} else if (player.y <= 3) {
 		screen_up();
-		player.score += 1;
 	} else {
 		player.y= player.y-1;
 	}
@@ -52,34 +46,20 @@ player_t move_left(player_t player)
 	}
 }
 
-//Display game over message with player score
-static void game_over_show(player_t player)
-{
-	
-	uint8_t num = player.score;
-	char str[5];
-	itoa(num, str, 10);
-	char msg[] = "GAME OVER SCORE ";
-	strcat(msg, str);
-	tinygl_text(msg);
-	while(1) {
-		pacer_wait();
-		tinygl_update();
-	}
-}
-
 player_t player_init(void)
 {
-	player_t player = {0, 4, 4};
+	player_t player = {4, 4};
 	return player;
 }
 
 
 //Displays player and check for collision
-void player_update(player_t player) {
-	if (!screen_pixel_get(player.y, player.x)) {
-		show_column(BIT(player.x), player.y);
+void player_update(player_t* player) {
+	if (!screen_pixel_get(player->y, player->x)) {
+		screen_show_column(BIT(player->x), player->y);
 	} else {
-		game_over_show(player);
+		screen_show_game_over();
+		player->x = 4;
+		player->y = 4;
 	}
 }
