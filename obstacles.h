efa079118de830882b99c6507bc6a63dc95d8bc7
Jeremy Russel Gatdula <jga137@uclive.ac.nz>
2021-10-21 13:18:53 +1300

    Added refresh obstacles refresh

--- obstacles.h
+++ obstacles.h
@@ -10,8 +10,10 @@
 void create_obstacle(uint8_t* bitmap);
 // Updates moving obstacles
 void obstacle_update(uint8_t* bitmap);
+// Removes all car placements
+void obstacle_refresh(void);
 // Refreshes placements and sets screen rate
-void obstacle_init(uint8_t rate);
+void obstacle_init(uint16_t rate);
 
 
 
