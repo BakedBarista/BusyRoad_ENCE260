f20db27759044ed90d63f42e35d307797708e400
Jeremy Russel Gatdula <jga137@uclive.ac.nz>
2021-10-21 13:18:22 +1300

    Made cars able to move right

--- obstacles.c
+++ obstacles.c
@@ -7,8 +7,8 @@
 static uint8_t screen_counter[6] = {0, 0, 2, 3, 2, 1};
 static uint8_t car_presets[] = {0x11, 0x54, 0x10, 0x14, 0x02, 0x08};
 static uint8_t car_times[SCREEN_WIDTH] = {0};
-static uint8_t car_placements;
-static uint8_t screen_rate;
+static uint16_t car_placements;
+static uint16_t screen_rate;
 
 // returns true/false if path is blocked
 static bool check_obstacle(uint8_t* bitmap, uint8_t check)
@@ -31,7 +31,7 @@ void create_obstacle(uint8_t* bitmap)
         car_times[SCREEN_WIDTH - i] = car_times[SCREEN_WIDTH - i - 1];
     }
     car_placements = car_placements << 1;
-    car_placements &= 0x7f;
+    car_placements &= 0x7f7f;
 
     uint8_t empty_row = screen_counter[rand() % 5];
     if (empty_row == 0) {
@@ -45,7 +45,12 @@ void create_obstacle(uint8_t* bitmap)
     } else if (rand() % 2 == 1) { // how often cars are made for every blank row
         // Creates moving cars
         bitmap[0] = car_presets[rand() % 6];
-        car_placements |= 0x01;
+
+        if (rand() % 2 == 1) {
+            car_placements |= 0x0001;
+        } else {
+            car_placements |= 0x0100;
+        }
         car_times[0] = (rand() % (CAR_SPEED*screen_rate));
 
     } else {
@@ -59,21 +64,36 @@ void obstacle_update(uint8_t* bitmap)
 {
     for (int i = 0; i < SCREEN_WIDTH; i++) {
         uint8_t time = car_times[i];
-        if ((car_placements & BIT(i)) != 0 && time/screen_rate >= CAR_SPEED) {
-            bitmap[i] = bitmap[i] << 1;
-            bitmap[i] &= 0x7f;
-            if ((bitmap[i] & 0x03) == 0 && (rand() % 3) == 1) {
-                bitmap[i] |= 0x01;
+        if (time/screen_rate >= CAR_SPEED) {
+            if ((car_placements & BIT(i)) != 0) {
+                bitmap[i] = bitmap[i] << 1;
+                bitmap[i] &= 0x7f;
+                if ((bitmap[i] & 0x03) == 0 && (rand() % 3) == 1) {
+                    bitmap[i] |= 0x01;
+                }
+                car_times[i] = 0;
+            } else if ((car_placements & BIT(i+8)) != 0) {
+                bitmap[i] = bitmap[i] >> 1;
+                if ((bitmap[i] & 0xc0) == 0 && (rand() % 3) == 1) {
+                    bitmap[i] |= 0x80;
+                }
+                car_times[i] = 0;
             }
-            car_times[i] = 0;
         }
-        car_times[i] += 1;
+        if (car_times[i] < 255) {
+            car_times[i] += 1;
+        }
     }
+}
+// Removes all car placements
+void obstacle_refresh(void)
+{
+    car_placements = 0x0000;
 }
 
 // Refreshes placements and sets screen rate
-void obstacle_init(uint8_t rate)
+void obstacle_init(uint16_t rate)
 {
-    car_placements = 0x00;
+    obstacle_refresh();
     screen_rate = rate;
 }
