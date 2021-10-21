c68e63a6132308ab48faff8be24cbbc458cf3402
Jeremy Russel Gatdula <jga137@uclive.ac.nz>
2021-10-21 13:22:50 +1300

    Added screen modes, moved game over function and updated old functions

--- screen.h
+++ screen.h
@@ -6,16 +6,36 @@
 #define SCREEN_WIDTH 5
 #define SCREEN_HEIGHT 7
 
+// Number of seconds that the player can idle to not lose
+#define SCREEN_TIME_OUT 12
+
+typedef enum 
+{
+    /** Showing title of game */
+    SCREEN_MODE_TITLE, 
+    /** Showing instructions  */
+    SCREEN_MODE_INSTRUCTIONS,
+    /** Showing game */
+    SCREEN_MODE_GAME,
+    /** Showing game over */
+    SCREEN_MODE_END,
+} screen_mode_t;
+
+
 // Lits given column with given pattern
-void show_column(uint8_t row_pattern, uint8_t current_column);
+void screen_show_column(uint8_t row_pattern, uint8_t current_column);
 // Displays the screen
 void screen_update(void);
 // Returns true/false if given pixel is lit
 bool screen_pixel_get(uint8_t col, uint8_t row);
 // Moves screen up
 void screen_up(void);
+// Unpauses the screen
+void screen_navswitch_pressed(void);
+//Display game over message with player score
+void screen_show_game_over(void);
 // Initialises screen/display
-void screen_init(uint8_t rate);
+void screen_init(uint16_t rate);
 
 #endif //SCREEN_H
 
